ca77cc11f15bd00e12cc552330c1b4d2b97ba3b7
Ullrich Rieger <vaporized@web.de>
2018-02-04 12:38:02 +0100

    added ares for low energy level

--- src/main/java/org/ully/enterprise/panel/LifeSupportPane.java
+++ src/main/java/org/ully/enterprise/panel/LifeSupportPane.java
@@ -4,9 +4,10 @@ import org.ully.enterprise.LifeSupport;
 import org.ully.enterprise.units.Power;
 
 import eu.hansolo.medusa.Gauge;
-import eu.hansolo.medusa.Gauge.LedType;
 import eu.hansolo.medusa.Gauge.SkinType;
 import eu.hansolo.medusa.GaugeBuilder;
+import eu.hansolo.medusa.Section;
+import eu.hansolo.medusa.SectionBuilder;
 import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -25,13 +26,21 @@ public class LifeSupportPane extends GridPane implements Refreshable {
     }
 
     private Gauge mkGauge() {
+
+        Section section = SectionBuilder.create().color(Color.CRIMSON).start(0).stop(2).build();
+
         gauge = GaugeBuilder.create().skinType(SkinType.GAUGE)//
+                .title(system.getName()).subTitle("life").unit(Power.SYMBOL).maxValue(system.getMaxPower().value()) //
+                .needleColor(Color.DARKBLUE)//
+/*
                 .ledVisible(true) //
                 .ledType(LedType.STANDARD) //
                 .ledColor(Color.color(1, 0, 0)) //
                 .ledBlinking(true) //
-                .ledOn(false) //
-                .title(system.getName()).subTitle("life").unit(Power.SYMBOL).maxValue(system.getMaxPower().value())
+                .ledOn(true) //
+ */
+                .areas(section) //
+                .areasVisible(true) //
                 .build();
         return gauge;
     }
@@ -39,8 +48,5 @@ public class LifeSupportPane extends GridPane implements Refreshable {
     @Override
     public void refresh() {
         gauge.setValue(system.getPower().value());
-        if (gauge.isLedOn() != system.getCurrentPowerFlow().value() <5) {
-            gauge.setLedOn(system.getCurrentPowerFlow().value() <5);
-        }
     }
 }
