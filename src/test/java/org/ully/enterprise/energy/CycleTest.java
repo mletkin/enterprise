2b4760febbf7b5bb4af58c5ab9fde70e3dc6c177
Ullrich Rieger <vaporized@web.de>
2018-02-04 13:51:43 +0100

    fixed unit tests

--- src/test/java/org/ully/enterprise/energy/CycleTest.java
+++ src/test/java/org/ully/enterprise/energy/CycleTest.java
@@ -1,7 +1,6 @@
 package org.ully.enterprise.energy;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.ully.enterprise.Component.Direction;
 import org.ully.enterprise.Phaser;
@@ -12,32 +11,36 @@ import org.ully.enterprise.units.Power;
 
 public class CycleTest {
 
-
-    @Ignore
     @Test
-    public void singleConsumer() {
-        Shield shield = new Shield("");
-        Circuit c = new Circuit(new Reactor("", Power.of(10)), shield);
+    public void singleConsumerWithoutEntropy() {
+        Phaser one = new Phaser("");
+        Reactor reactor = mkReactor(Power.of(1));
+        Circuit c = new Circuit(reactor, one);
 
         new Cycle(c).calculate(1000);
-        Assert.assertEquals(1.0, shield.getLoad().value(), 0.1d);
+        Assert.assertEquals(1.0, one.getLoad().value(), 0.1d);
     }
 
-    @Ignore
+    private Reactor mkReactor(Power flow) {
+        Reactor reactor = new Reactor("", Power.of(10));
+        reactor.setFlow(flow);
+        return reactor;
+    }
+
     @Test
-    public void twoShields() {
-        Shield shield1 = new Shield("");
-        shield1.load(Energy.of(1), 1000);
-        shield1.setDirection(Direction.OUT);
+    public void unloadOneSheldToTheOther() {
+        Shield src = new Shield("");
+        src.load(Energy.of(10), 1000);
+        src.setDirection(Direction.OUT);
 
-        Shield shield2 = new Shield("");
-        shield2.setDirection(Direction.IN);
+        Shield dst = new Shield("");
+        dst.setDirection(Direction.IN);
 
-        Circuit c = new Circuit(shield1, shield2);
-
+        Circuit c = new Circuit(src, dst);
         new Cycle(c).calculate(1000);
-        Assert.assertEquals(0.0, shield1.getLoad().value(), 0.1d);
-        Assert.assertEquals(1.0, shield2.getLoad().value(), 0.1d);
+
+        Assert.assertEquals(3.0, src.getLoad().value(), 0.1d);
+        Assert.assertEquals(4.0, dst.getLoad().value(), 0.1d);
     }
 
     @Test
@@ -56,29 +59,28 @@ public class CycleTest {
         Assert.assertEquals(1.0, dest.getLoad().value(), 0.1d);
     }
 
-    //
-//    @Ignore
-//    @Test
-//    public void twoConsumersWithSufficientPowerSupply() {
-//        Circuit c = new Circuit();
-//        c.supplier = Arrays.asList(new Reactor("", Power.of(10)));
-//        c.consumer = Arrays.asList(new Shield(""), new Shield(""));
-//
-//        new Cycle(c).calculate(1000);
-//        Assert.assertEquals(1.0, c.consumer.get(0).getLoad().value(), 0.1d);
-//        Assert.assertEquals(1.0, c.consumer.get(1).getLoad().value(), 0.1d);
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void twoConsumersWith50PercentPowerSupply() {
-//        Circuit c = new Circuit();
-//        c.supplier = Arrays.asList(new Reactor("", Power.of(1)));
-//        c.consumer = Arrays.asList(new Shield(""), new Shield(""));
-//
-//        new Cycle(c).calculate(1000);
-//        Assert.assertEquals(.5, c.consumer.get(0).getLoad().value(), 0.001d);
-//        Assert.assertEquals(.5, c.consumer.get(1).getLoad().value(), 0.001d);
-//    }
+    @Test
+    public void twoConsumersWithSufficientPowerSupply() {
+        Shield s1 = new Shield("");
+        Phaser p1 = new Phaser("");
+        Circuit c = new Circuit(mkReactor(Power.of(10)), s1, p1);
+
+        new Cycle(c).calculate(1000);
+
+        Assert.assertEquals(4.0, s1.getLoad().value(), 0.1d);
+        Assert.assertEquals(5.0, p1.getLoad().value(), 0.1d);
+    }
+
+    @Test
+    public void twoConsumersWith50PercentPowerSupply() {
+        Shield s1 = new Shield("");
+        Phaser p1 = new Phaser("");
+        Circuit c = new Circuit(mkReactor(Power.of(5)), s1, p1);
+
+        new Cycle(c).calculate(1000);
+
+        Assert.assertEquals(1.5, s1.getLoad().value(), 0.001d);
+        Assert.assertEquals(2.5, p1.getLoad().value(), 0.001d);
+    }
 
 }
