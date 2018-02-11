fe81b9233ef4acdb621be21db7b1b835cd23d5ff
Ullrich Rieger <vaporized@web.de>
2018-02-11 15:15:41 +0100

    added comments

--- src/main/java/org/ully/enterprise/panel/energy/GatewayPane.java
+++ src/main/java/org/ully/enterprise/panel/energy/GatewayPane.java
@@ -11,17 +11,32 @@ import eu.hansolo.medusa.GaugeBuilder;
 import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
 
+/**
+ * Panel for a circuit energy exchange gateway.
+ * <ul>
+ * <li>Shows the energy flowing from or to the circuit through the gateway.
+ * <li>When energy flows OUT of the circuit, the needle goes left.
+ * <li>When energy flows INto the circuit the needle goes right.
+ * <li>The scale is logarithmic from the center to the border.
+ * </ul>
+ * The scales maximum value is fixed at +/- 5 so the maximum power that may flow
+ * would be around 148 Units.
+ */
 public class GatewayPane extends GridPane implements Refreshable {
 
     private static final int MAX = 5;
     private Gauge gauge;
     private PowerGateway gateway;
 
+    /**
+     * Ceates a pane for the given gateway.
+     *
+     * @param gateway
+     */
     public GatewayPane(PowerGateway gateway) {
         super();
         this.gateway = gateway;
         setAlignment(Pos.CENTER);
-
         add(mkGauge(), 0, 0);
     }
 
@@ -37,10 +52,16 @@ public class GatewayPane extends GridPane implements Refreshable {
         double value = gateway.getCurrentPowerFlow().equals(Power.ZERO) //
                 ? 0
                 : (gateway.getDirection() == Direction.IN ? -1 : 1) * gateway.getCurrentPowerFlow().value();
-        gauge.setValue(transform(value));
+        gauge.setValue(scale(value));
     }
 
-    private double transform(double value) {
+    /**
+     * Scale the signed energy value to the logarithmic scale.
+     *
+     * @param value
+     * @return
+     */
+    private double scale(double value) {
         return Math.signum(value) * (Math.log(1 + Math.abs(value)));
     }
 }
