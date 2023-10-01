86ef9f30c48c040d7e7564dcd445531732355a9a
mletkin <vaporized@web.de>
2023-10-01 15:25:11 +0200

    Update to JDK 11, pmd removed

---
+++ src/main/java/org/ully/enterprise/LaunchPad.java
@@ -0,0 +1,19 @@
+package org.ully.enterprise;
+
+import org.ully.enterprise.panel.Dashboard;
+
+/**
+ * Main class to launch the starship
+ */
+public class LaunchPad {
+
+    /**
+     * Launches the enterprise.
+     *
+     * @param args
+     *                 simply ignored
+     */
+    public static void main(String... args) {
+        Dashboard.main(args);
+    }
+}
